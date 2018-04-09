1f0f117762c8d048748a94988b7dbeb6d3175c81
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-04-09 12:01:13 -0500

    Botones para likes OK

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,4 +12,20 @@ export class MainComponent implements OnInit {
   ngOnInit() {
   }
 
+  public sumarLike(event){
+    var likes = <HTMLInputElement>document.getElementById("num_likes");
+    var num_likes = parseInt(likes.value);
+    console.log(num_likes);
+    num_likes= num_likes+1;
+    likes.value = num_likes.toString();
+  }
+
+  public restarLike(event){
+    var likes = <HTMLInputElement>document.getElementById("num_likes");
+    var num_likes = parseInt(likes.value);
+    console.log(num_likes);
+    num_likes= num_likes-1;
+    likes.value = num_likes.toString();
+  }
+
 }
