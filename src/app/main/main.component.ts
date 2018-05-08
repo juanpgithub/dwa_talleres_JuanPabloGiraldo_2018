fd1ffa637f41d14fa285428afb1b6cba515bfc61
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-05-07 23:48:40 -0500

    Imprimir nombre del pokemon en main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { PeticionService } from '../services/peticion.service';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  providers: [PeticionService]
 })
 export class MainComponent implements OnInit {
-
+  public lista_pokemones;
   public parametro: string;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _peticionService: PeticionService
+
   ) { }
 
   ngOnInit() {
+     // Llamando al servicio _peticionService 
+     this._peticionService.getArticulos().subscribe(
+      result => {
+        console.log(result);
+        this.lista_pokemones = result;
+
+      },
+      error => {
+        var errorMsj = <any>error;
+        console.log(errorMsj);
+      }
+    );
+    
     this._route.params.forEach((params:Params) => {
       this.parametro = params['ident'];
       console.log(params);
